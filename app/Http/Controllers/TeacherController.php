bf78d9c930bf4ce5ced5897e0318fe686aeba475
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-06-20 11:59:21 +0700

    add class list in teacher's dashboard

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TeacherController extends Controller
 {
     public function index(){
-        return view('home');
+        $teacher = User::where('userID', Auth::user()->userID)->first();
+        $classes = $teacher->my_classes;
+        return view('teacher.index')->with([
+            'teacher'=>$teacher,
+            'classes'=>$classes
+        ]);
     }
 }
