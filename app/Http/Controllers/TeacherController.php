0463a1496b755ed4d4ecff5c71c12639e3fe1c62
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-06-26 16:58:17 +0700

    add warning for disabled accounts

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -8,8 +8,13 @@ use Illuminate\Support\Facades\Auth;
 
 class TeacherController extends Controller
 {
-    public function index(){
+    public function index(Request $rq){
         $teacher = User::where('userID', Auth::user()->userID)->first();
+        if($teacher->status == 0){
+            $rq->session()->put('error', 'Your account is disabled. Contact admin for more info');
+            Auth::logout();
+            return redirect(url('/login'));
+        }
         $classes = $teacher->my_classes;
         return view('teacher.index')->with([
             'teacher'=>$teacher,
