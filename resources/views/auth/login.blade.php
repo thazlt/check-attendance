3267ce691ce164dba24177512b2c659228ea4bf4
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-06-26 16:58:17 +0700

    add warning for disabled accounts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,11 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
+                        @if (session('error'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ session()->pull('error') }}
+                        </div>
+                        @endif
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
