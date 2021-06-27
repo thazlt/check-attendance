688eaf6d0991411b8818d0d990413e46108f96cc
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-06-28 00:09:03 +0700

    add student - class relationship

---
+++ app/Models/StudentClass.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentClass extends Model
+{
+    use HasFactory;
+    protected $table = "student_class";
+    protected $fillable = [
+        'studentID',
+        'classID',
+    ];
+}
