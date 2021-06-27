3736a0b3cb6e57e1d2919e53a8d46d8ee3f36f29
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-06-28 00:55:20 +0700

    add schedule for class function

--- resources/views/admin/classmanage/viewClass.blade.php
+++ resources/views/admin/classmanage/viewClass.blade.php
@@ -1,56 +1,184 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __($class->name) }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session()->pull('status') }}
-                        </div>
-                    @endif
-                    <h4>Teachers:</h4>
-                    @foreach ($class->teachers as $teacher)
-                        <p>{{ $teacher->name }}</p>
-                        <form action="{{ url('/admin/removeTeacher') }}" method="post">
-                            @csrf
-                            <input type="hidden" name="classID" value="{{ $class->classID }}">
-                            <input type="hidden" name="userID" value="{{ $teacher->userID }}">
-                            <button type="submit">Remove</button>
-                        </form>
-                    @endforeach
-                    @if ($class->status == 1)
-                        <a href="{{ url('/admin/addTeacher/'.$class->classID) }}">Add another teacher</a><br>
-                        <form action="{{ url('/admin/deactivateClass') }}" method="post">
-                            @csrf
-                            <input type="hidden" name="classID" value="{{ $class->classID }}">
-                            <button type="submit">Deactivate Class</button>
-                        </form>
-                    @else
-                    <form action="{{ url('/admin/activateClass') }}" method="post">
-                        @csrf
-                        <input type="hidden" name="classID" value="{{ $class->classID }}">
-                        <button type="submit">Activate Class</button>
-                    </form>
-                    @endif
-                    <h4>Status: {{ $class->status == 1 ? "Active" : "Disabled" }}</h4>
-                    <h4>Time Table</h4>
-                    <table>
-                        <tr>
-                            <th>Students</th>
-                            <th>Name</th>
-                            @foreach ($class->schedules() as $schedule)
-                                <th>{{ $schedule->date }}</th>
-                            @endforeach
-                        </tr>
-                    </table>
+<div class="main-content">
+        <header>
+            <h2>
+                <label for="nav-toggle">
+                    <span class="fa fa-bars"></span>
+                </label> Check Attendance
+            </h2>
+            <div class="search-wrapper">
+                <span class="fa fa-search"></span>
+                <input type="search" placeholder="Search here">
+            </div>
+            <div class="user-wrapper">
+                <img src="../pics/user.svg" width="30px" height="30px" alt="">
+                <div>
+                    <h4>Vinny Ng</h4>
+                    <small>Teacher</small>
                 </div>
             </div>
-        </div>
+        </header>
+
+        <main>
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session()->pull('status') }}
+                </div>
+            @endif
+            <div class="cards">
+                <form action="{{ url('/admin/addTeacher') }}" method='post'>
+                    @csrf
+                    <div class="form-group row">
+                        <label for="teacherID" class="col-md-4 col-form-label text-md-right">{{ __('Add new Teacher') }}</label>
+                        <input id="teacherID" type="text" class="form-control @error('teacherID') is-invalid @enderror" name="teacherID" required placeholder="Teacher ID">
+                            @error('teacherID')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                    </div>
+                    <input type="hidden" name="classID" value="{{ $class->classID }}">
+                    <div class="form-group">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Add') }}
+                            </button>
+                    </div>
+                </form>
+                <form action="{{ url('/admin/addStudentClass') }}" method='post'>
+                    @csrf
+                    <div class="form-group row">
+                        <label for="studentID" class="col-md-4 col-form-label text-md-right">{{ __('Add new Student') }}</label>
+                        <input id="studentID" type="text" class="form-control @error('studentID') is-invalid @enderror" name="studentID" required placeholder="Student ID">
+                            @error('studentID')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                    </div>
+                    <div class="form-group">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Add') }}
+                            </button>
+                    </div>
+                    <input type="hidden" name="classID" value="{{ $class->classID }}">
+                </form>
+                <form action="{{ url('/admin/addSchedule') }}" method='post'>
+                    @csrf
+                    <div class="form-group row">
+                        <label for="date" class="col-md-4 col-form-label text-md-right">{{ __('Add a Date') }}</label>
+                        <input id="date" type="text" class="form-control @error('date') is-invalid @enderror" name="date" required placeholder="Date">
+                            @error('date')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                    </div>
+                    <div class="form-group">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Add') }}
+                            </button>
+                    </div>
+                    <input type="hidden" name="classID" value="{{ $class->classID }}">
+                </form>
+            </div>
+            <div class="cards">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h5>CLASS: {{ $class->name }}</h5>
+                        </div>
+                        <div class="col-md-6">
+                            <h5>LECTURER:
+                                @foreach ($class->teachers as $teacher)
+                                    <span>{{ $teacher->name }}<button class="btn btn-outline" type="submit" form="rmt-{{ $class->classID.'-'.$teacher->userID }}"><i class="fa fa-trash"></i></button></span>
+                                    <form id="rmt-{{ $class->classID.'-'.$teacher->userID }}" action="{{ url('admin/removeTeacher') }}" method="post">
+                                        @csrf
+                                        <input type="hidden" name="classID" value="{{ $class->classID }}">
+                                        <input type="hidden" name="userID" value="{{ $teacher->userID }}">
+                                    </form>
+                                @endforeach
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="card-single">
+                        <h5>V: PRESENT</h5>
+                        <h5>L: Late</h5>
+                        <h5>A: Absent</h5>
+                        <h5>P: Absent with Permision</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="cards">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <table class="content-table">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>ID</th>
+                                        <th>Last name</th>
+                                        <th>First Name</th>
+                                        <th>Check</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $num = 1;?>
+                                    @foreach ($class->students as $student)
+                                    <tr>
+                                        <td>{{ $num }}</td>
+                                        <td>{{ $student->studentID }}</td>
+                                        <?php
+                                        $name = explode(" ",$student->name);
+                                        $firstName = $name[count($name) - 1];
+                                        $lastName = substr($student->name, 0, strlen($student->name) - strlen($firstName));
+                                        ?>
+                                        <td>{{ $lastName }}</td>
+                                        <td>{{ $firstName }}</td>
+                                        <td><button class="btn btn-danger">A</button>
+                                            <button class="btn btn-primary">P</button>
+                                            <button class="btn btn-success">V</button>
+                                            <button class="btn btn-warning">L</button></td>
+                                        </td>
+                                        <td>
+                                            <form id="rms-{{ $class->classID }}-{{ $student->studentID }}" action="{{ url('admin/removeStudentClass') }}" method="post">
+                                                @csrf
+                                                <input type="hidden" name="classID" value="{{ $class->classID }}">
+                                                <input type="hidden" name="studentID" value="{{ $student->studentID }}">
+                                            </form>
+                                            <button class="btn btn-outline" type="submit" form="rms-{{ $class->classID }}-{{ $student->studentID }}"><i class="fa fa-trash"></i></button>
+                                        </td>
+                                    </tr>
+                                    <?php $num++?>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col-sm-6" style="overflow-x:auto !important;">
+                            <table class="content-table">
+                                <thead>
+                                    <tr>
+                                        @foreach ($class->schedules->sortBy('date') as $schedule)
+                                           <td>{{ $schedule->date }}</td>
+                                        @endforeach
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        @foreach ($class->schedules->sortBy('date') as $schedule)
+                                            <td><button class="btn btn-success">V</button></td>
+                                        @endforeach
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </main>
     </div>
-</div>
 @endsection
