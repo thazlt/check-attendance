aacf74ee813b867b347efb2bda2e2c312a9a95f1
thazlt <1859044@itec.hcmus.edu.vn>
2021-07-03 17:31:50 +0700

    add buttons in attendance form

--- resources/views/include/attendanceFrom.blade.php
+++ resources/views/include/attendanceFrom.blade.php
@@ -1,8 +1,41 @@
-<form id="{{ 'check-'.$scheduleID.'-'.$studentID }}" action="{{ url('/checkAttendance') }}" method="POST">
+<form id="{{ 'form-check-'.$scheduleID.'-'.$studentID }}" action="{{ url('/checkAttendance') }}" method="POST">
     @csrf
     @method('put')
     <input type="hidden" name="scheduleID" value="{{ $scheduleID }}">
     <input type="hidden" name="studentID" value="{{ $studentID }}">
-    <input type="hidden" name="status" value="{{ $status }}">
+    <input type="hidden" name="status" value="
+    @switch($status)
+        @case('A')
+            P
+            @break
+        @case('P')
+            V
+            @break
+        @case('V')
+            L
+            @break
+        @case('L')
+            A
+            @break
+        @default
+
+    @endswitch
+    ">
 </form>
-<button class="btn btn-danger" type="submit" form="{{ 'check-'.$scheduleID.'-'.$studentID }}">A</button>
+@switch($status)
+        @case('A')
+            <button class="btn btn-danger" type="submit" form="{{ 'form-check-'.$scheduleID.'-'.$studentID }}">A</button>
+            @break
+        @case('P')
+            <button class="btn btn-warning" type="submit" form="{{ 'form-check-'.$scheduleID.'-'.$studentID }}">P</button>
+            @break
+        @case('V')
+            <button class="btn btn-success" type="submit" form="{{ 'form-check-'.$scheduleID.'-'.$studentID }}">V</button>
+            @break
+        @case('L')
+            <button class="btn btn-info" type="submit" form="{{ 'form-check-'.$scheduleID.'-'.$studentID }}">L</button>
+            @break
+        @default
+
+    @endswitch
+
