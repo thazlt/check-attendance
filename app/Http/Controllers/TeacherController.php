06343b80843963c601216396e61e006348f6837c
thazlt <1859044@itec.hcmus.edu.vn>
2021-07-03 22:22:55 +0700

    fix

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -35,7 +35,7 @@ class TeacherController extends Controller
 
     public function viewClass(Request $rq){
         $class = MyClass::where('classID', $rq->id)->first();
-        return view('teacher.classmanage.viewClass')->with([
+        return view('teacher.classmanage.viewclass')->with([
             'class'=>$class
         ]);
     }
